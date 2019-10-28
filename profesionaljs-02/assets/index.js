0bcdb5fba5588efb65f90513fccd925364b447c5
Massimo Di Berardino <maxdo1996@gmail.com>
2019-10-28 16:16:12 -0300

    arreglo de carpetas

---
+++ profesionaljs-02/assets/index.js
@@ -0,0 +1,12 @@
+import AutoPlay from '../plugins/AutoPlay.js';
+import MediaPlayer from './MediaPlayer.js';
+
+//const { MediaPlayer } = require('./MediaPlayer.js');
+
+const video = document.querySelector("video");
+const play = document.getElementsByClassName('play');
+const mute = document.getElementsByClassName('mute');
+
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay] });
+play.onclick = () => player.togglePlay();
+mute.onclick = () => player.toggleMute();
