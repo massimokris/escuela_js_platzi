e2dbfd354c3b36d41dc2b885d08e190f5a9fcd1c
Massimo Di Berardino <maxdo1996@gmail.com>
2019-10-29 16:17:14 -0300

    herencia prototipal y promesas

--- profesionaljs-02/assets/plugins/AutoPlay.js
+++ profesionaljs-02/assets/plugins/AutoPlay.js
@@ -1,8 +1,11 @@
 function AutoPlay() {}
 
 AutoPlay.prototype.run = function (player) {
-    player.mute();
-    player.play();
+    if(!player.muted) {
+        player.muted = true;
+        player.play();
+    }
+    
 }
 
 module.exports = AutoPlay;
