5e81905a61c1cfa56114c922afc4362e0afdb186
Massimo Di Berardino <maxdo1996@gmail.com>
2019-11-04 09:50:32 -0300

    generadores y proxys

--- profesionaljs-02/assets/plugins/AutoPlay.js
+++ profesionaljs-02/assets/plugins/AutoPlay.js
@@ -3,9 +3,8 @@ function AutoPlay() {}
 AutoPlay.prototype.run = function (player) {
     if(!player.muted) {
         player.muted = true;
-        player.play();
     }
-    
+    player.play();
 }
 
 module.exports = AutoPlay;
