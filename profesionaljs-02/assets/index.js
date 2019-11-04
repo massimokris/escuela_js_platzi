b2a0d92ed1c2dfa3e77c201b3ae56e743c721e68
Massimo Di Berardino <maxdo1996@gmail.com>
2019-11-04 17:13:47 -0300

    abort-fetch/intersectionObserver/VisibilityChange

--- profesionaljs-02/assets/index.js
+++ profesionaljs-02/assets/index.js
@@ -1,4 +1,5 @@
 import AutoPlay from './plugins/AutoPlay.js';
+import Autopause from './plugins/AutoPause.js';
 import MediaPlayer from './MediaPlayer.js';
 
 //const { MediaPlayer } = require('./MediaPlayer.js');
@@ -7,7 +8,7 @@ const video = document.querySelector("video");
 const play = document.getElementsByClassName('play');
 const mute = document.getElementsByClassName('mute');
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay, new Autopause] });
 play.onclick = () => player.togglePlay();
 mute.onclick = () => player.toggleMute();
 
