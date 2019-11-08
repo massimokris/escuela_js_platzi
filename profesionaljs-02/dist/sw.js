970b00587175a6a86fc63328ccf6541709e95484
Massimo Di Berardino <maxdo1996@gmail.com>
2019-11-08 17:28:11 -0300

    patrones de diseño y mas typescript

--- profesionaljs-02/dist/sw.js
+++ profesionaljs-02/dist/sw.js
@@ -186,7 +186,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63879" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54858" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
