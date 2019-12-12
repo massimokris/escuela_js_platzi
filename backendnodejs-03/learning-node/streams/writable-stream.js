43baec00ba6314e0b99fcdf89a178e61f7386443
Massimo Di Berardino <maxdo1996@gmail.com>
2019-12-12 17:35:12 -0300

    streams y so

--- backendnodejs-03/learning-node/streams/writable-stream.js
+++ backendnodejs-03/learning-node/streams/writable-stream.js
@@ -1,5 +1,10 @@
 const { Writable } = require('stream');
 
 const writableStream = new Writable({
-    write(chunk, encoding)
-})
+    write(chunk, encoding, callback) {
+        console.log(chunk.toString());
+        callback();
+    }
+});
+
+process.stdin.pipe(writableStream);
