992bd774a8b1ec5034e8a13ab9c3fccf67c30e52
Massimo Di Berardino <maxdo1996@gmail.com>
2019-12-25 13:34:11 -0300

    agregando gitignore y resto de metodos base del crud

--- backendnodejs-03/movies-api/routes/movies.js
+++ backendnodejs-03/movies-api/routes/movies.js
@@ -16,6 +16,54 @@ function moviesApi(app) {
             next(err);
         }
     });
+
+    router.get('/:movieId', async (req, res, next) => {
+        try {
+            const movie = await Promise.resolve(moviesMock[0]);
+            res.status(200).json({
+                data: movie,
+                message: 'Movie retrived'
+            })
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.post('/', async (req, res, next) => {
+        try {
+            const createMovieId = await Promise.resolve(moviesMock[0].id);
+            res.status(201).json({
+                data: createMovieId,
+                message: 'Movie created'
+            })
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.put('/:movieId', async (req, res, next) => {
+        try {
+            const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+            res.status(200).json({
+                data: updatedMovieId,
+                message: 'Movie updated'
+            })
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.delete('/:movieId', async (req, res, next) => {
+        try {
+            const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+            res.status(200).json({
+                data: deletedMovieId,
+                message: 'Movie deleted'
+            })
+        } catch (err) {
+            next(err);
+        }
+    });
 } 
 
 module.exports = moviesApi;
